fcedd074e8efea25a226c78a154cf268be4bb995
Devansh Singh Rathore <rathoredevansh08@gmail.com>
2019-06-06 01:14:02 +0530

    .cpp template

---
+++ temp.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+#define inf 1000000007
+#define pb push_back
+#define for(i,a,b) for(int i=a;i<=b;i++) 
+typedef long long int ll;
+typedef vector<int> vi;
+
+int t,m,n,a,b,f=0;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	
+	return 0;
+}
