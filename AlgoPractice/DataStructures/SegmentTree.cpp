4b28360c2761ec44fad2a9d7daddbda9195c670b
DSR <rathoredevansh08@gmail.com>
2020-04-17 15:11:13 +0530

    lazy propagation added

--- AlgoPractice/DataStructures/SegmentTree.cpp
+++ AlgoPractice/DataStructures/SegmentTree.cpp
@@ -13,6 +13,7 @@ number of levels is O(Logn).
 */
 
 int getMid(int s, int e) { return s + (e -s)/2; }  
+int lazy[1000] = {0};  // To store pending updates 
 
 int getSumUtil(int *st, int ss, int se, int qs, int qe, int si)  
 {  
@@ -30,6 +31,46 @@ int getSumUtil(int *st, int ss, int se, int qs, int qe, int si)
     return getSumUtil(st, ss, mid, qs, qe, 2*si+1) +  
         getSumUtil(st, mid+1, se, qs, qe, 2*si+2);  
 }  
+
+int getSumUtil_Lazy(int *st, int ss, int se, int qs, int qe, int si) 
+{ 
+    // If lazy flag is set for current node of segment tree, then there are some pending updates. So we need to 
+    // make sure that the pending updates are done before processing the sub sum query 
+    if (lazy[si] != 0) 
+    { 
+        // Make pending updates to this node. Note that this node represents sum of elements in arr[ss..se] and 
+        // all these elements must be increased by lazy[si] 
+        st[si] += (se-ss+1)*lazy[si]; 
+  
+        // checking if it is not leaf node because if it is leaf node then we cannot go further 
+        if (ss != se) 
+        { 
+            // Since we are not yet updating children os si, we need to set lazy values for the children 
+            lazy[si*2+1] += lazy[si]; 
+            lazy[si*2+2] += lazy[si]; 
+        } 
+  
+        // unset the lazy value for current node as it has been updated 
+        lazy[si] = 0; 
+    } 
+  
+    // Out of range 
+    if (ss>se || ss>qe || se<qs) 
+        return 0; 
+  
+    // At this point we are sure that pending lazy updates are done for current node. So we can return value  
+    // (same as it was for query in our previous post) 
+  
+    // If this segment lies in range 
+    if (ss>=qs && se<=qe) 
+        return st[si]; 
+  
+    // If a part of this segment overlaps with the given range 
+    int mid = (ss + se)/2; 
+    return getSumUtil_Lazy(st, ss, mid, qs, qe, 2*si+1) + 
+           getSumUtil_Lazy(st, mid+1, se, qs, qe, 2*si+2); 
+} 
+
 
 void updateValueUtil(int *st, int ss, int se, int i, int diff, int si)  
 {  
@@ -64,6 +105,85 @@ void updateValue(int arr[], int *st, int n, int i, int new_val)
   
     updateValueUtil(st, 0, n-1, i, diff, 0);  
 }
+
+// update range without lazy propagation
+void updateRange(int *st, int ss, int se, int us, int ue, int diff, int si) 
+{ 
+    // out of range 
+    if (ss>se || ss>ue || se<us) 
+        return ; 
+  
+    // Current node is a leaf node 
+    if (ss==se) 
+    { 
+        st[si] += diff; 
+        return; 
+    } 
+  
+    // If not a leaf node, recur for children. 
+    int mid = (ss+se)/2; 
+    updateRange(st, ss, mid, us, ue, diff, si*2+1); 
+    updateRange(st, mid+1, se, us, ue, diff, si*2+2); 
+  
+    // Use the result of children calls to update this node 
+    st[si] = st[si*2+1] + st[si*2+2]; 
+}
+
+
+// update range with lazy propagation
+void updateRange_Lazy(int *st, int ss, int se, int us, int ue, int diff, int si) 
+{ 
+    // If lazy value is non-zero for current node of segment tree, then there are some pending updates. So we need 
+    // to make sure that the pending updates are done before making new updates. Because this value may be used by 
+    // parent after recursive calls (See last line of this function) 
+    if (lazy[si] != 0) 
+    { 
+        // Make pending updates using value stored in lazy nodes 
+        st[si] += (se-ss+1)*lazy[si]; 
+  
+        // checking if it is not leaf node because if it is leaf node then we cannot go further 
+        if (ss != se) 
+        { 
+            // We can postpone updating children we don't need their new values now. 
+            // Since we are not yet updating children of si, we need to set lazy flags for the children 
+            lazy[si*2 + 1]   += lazy[si]; 
+            lazy[si*2 + 2]   += lazy[si]; 
+        } 
+  
+        // Set the lazy value for current node as 0 as it has been updated 
+        lazy[si] = 0; 
+    } 
+  
+    // out of range 
+    if (ss>se || ss>ue || se<us) 
+        return; 
+  
+    // Current segment is fully in range 
+    if (ss>=us && se<=ue) 
+    { 
+        // Add the difference to current node 
+        st[si] += (se-ss+1)*diff; 
+  
+        // same logic for checking leaf node or not 
+        if (ss != se) 
+        { 
+            // This is where we store values in lazy nodes, rather than updating the segment tree itelf 
+            // Since we don't need these updated values now we postpone updates by storing values in lazy[] 
+            lazy[si*2 + 1]   += diff; 
+            lazy[si*2 + 2]   += diff; 
+        } 
+        return; 
+    } 
+  
+    // If not completely in rang, but overlaps, recur for children, 
+    int mid = (ss+se)/2; 
+    updateRange_Lazy(st, ss, mid, us, ue, diff, si*2+1); 
+    updateRange_Lazy(st, mid+1, se, us, ue, diff, si*2+2); 
+  
+    // And use the result of children calls to update this node 
+    st[si] = st[si*2+1] + st[si*2+2]; 
+} 
+
 
 int getSum(int *st, int n, int qs, int qe)  
 {  
